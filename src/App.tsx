74b630fc7efa77a5ecfc30a8a06652923e089e08
kleber <kleber107@gmail.com>
2021-04-04 18:23:28 -0300

    Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
 import { useState } from "react";
-import Modal from "react-modal"; //importando modal.
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
-Modal.setAppElement("#root"); //mostrando qual tag será bloqueada.
+Modal.setAppElement("#root");
 
 export function App() {
-	//controlando estado do modal. Aberto ou fechado.
 	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
 		false
 	);
 
-	//função responsável por abrir o modal.
 	function handleOpenNewTransactoinModal() {
 		setIsNewTransactionModalOpen(true);
 	}
-	//função responsável por fechar o modal.
+
 	function handleCloseNewTransactionModal() {
 		setIsNewTransactionModalOpen(false);
 	}
 
 	return (
 		<>
-			{/* esse modal será controlado no header. 
-			então tenho que passar como prop a função resposavel por abrir o modal.  */}
 			<Header onOpenNewTransactionModal={handleOpenNewTransactoinModal} />
 			<Dashboard />
 
 			<Modal
-				//informando se o modal está aberto ou fechado.
 				isOpen={isNewTransactionModalOpen}
-				//passando função responsavel por fechar o modal.
 				onRequestClose={handleCloseNewTransactionModal}
 			>
 				<h2>Cadastrar Transação</h2>
