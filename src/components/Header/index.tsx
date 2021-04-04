a85d3b068c48dd0bd8e1b2b7607a4dcde936f000
kleber <kleber107@gmail.com>
2021-04-04 18:22:25 -0300

    Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Contant } from "./styles";
 
-export function Header() {
+interface HeaderPros {
+	onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderPros) {
 	return (
 		<Container>
 			<Contant>
 				<img src={logoImg} alt="dt money" />
 
-				<button type="button">Nova transação</button>
+				<button type="button" onClick={onOpenNewTransactionModal}>
+					Nova transação
+				</button>
 			</Contant>
 		</Container>
 	);
