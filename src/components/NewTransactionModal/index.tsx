fbecb1077efb613d60f373e73bd8536a92c275a5
kleber <kleber107@gmail.com>
2021-04-05 22:09:46 -0300

    estilização input finalizada.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
-import { Container } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 Modal.setAppElement("#root");
 
@@ -13,6 +17,8 @@ export function NewTransactionModal({
 	isOpen,
 	onRequestClose,
 }: NewTransactionModalProps) {
+	const [type, setType] = useState("deposit");
+
 	return (
 		<Modal
 			isOpen={isOpen}
@@ -32,6 +38,32 @@ export function NewTransactionModal({
 
 				<input type="text" placeholder="Título" />
 				<input type="number" placeholder="Valor" />
+
+				<TransactionTypeContainer>
+					<RadioBox
+						type="button"
+						onClick={() => {
+							setType("deposit");
+						}}
+						isActive={type === "deposit"}
+						activeColor="green"
+					>
+						<img src={incomeImg} alt="Entrada" />
+						<span>Entrada</span>
+					</RadioBox>
+					<RadioBox
+						type="button"
+						onClick={() => {
+							setType("withdraw");
+						}}
+						isActive={type === "withdraw"}
+						activeColor="red"
+					>
+						<img src={outcomeImg} alt="Saída" />
+						<span>Saída</span>
+					</RadioBox>
+				</TransactionTypeContainer>
+
 				<input type="text" placeholder="Categoria" />
 				<button type="submit">Cadastrar</button>
 			</Container>
